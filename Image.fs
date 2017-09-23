fe215ea78874f41ae1817c2607ec1fa92a5a83f9
Anton Burger <burgeranton@gmail.com>
2017-09-23 06:21:35 +0100

    Refactor to begin to support multiple rays per pixel

--- Image.fs
+++ Image.fs
@@ -26,23 +26,45 @@ let write b fn =
     writer.WriteLine("255")
     writePixels b.pixels
 
-let generateRays camera resolution =
+type Frame = { i : Vector; j : Vector; k : Vector }
+
+let fromCamera c =
     // Camera's a left-handed coordinate system (+x right, +y up, +z forward). So right = up x forward rather than forward x up.
-    let forward = subP camera.lookAt camera.o |> normalise
-    let right = cross camera.up forward |> normalise
-    let up = cross forward right |> normalise
-    let ahead = addP camera.o forward
+    let k = subP c.lookAt c.o |> normalise
+    let i = cross c.up k |> normalise
+    let j = cross k i
+    { i = i; j = j; k = k }
+
+type PixelGrid = {
+    centre: Point;
+    i: Vector;
+    j: Vector;
+    resolution: Resolution;
+    pixelSize: float * float;
+    pixelCentres: (float * float) [,];
+}
+
+let createGrid camera resolution =
+    let frame = fromCamera camera
+    let ahead = addP camera.o frame.k
     let halfY = System.Math.Tan(camera.fovY / 2.0)
     let halfX = halfY * camera.aspectRatio
     let incY = (halfY * 2.0) / (resH resolution - 1 |> float)
     let incX = (halfX * 2.0) / (resV resolution - 1 |> float)
-    let offsets = Seq.unfold (fun (x, y) ->
-        let makeOffsets = (-halfX + float x * incX + incX / 2.0, -halfY + float y * incY + incY / 2.0)
-        let makeState = if x + 1 < resH resolution then (x + 1, y) else (0, y + 1)
-        if y < resV resolution
-            then Some (makeOffsets, makeState)
-            else None) (0, 0)
+    let offsets = Array2D.init (resH resolution) (resV resolution) (fun y x ->
+        (-halfX + float x * incX + incX / 2.0, halfY - float y * incY - incY / 2.0))
+    {
+        centre = ahead;
+        i = frame.i;
+        j = frame.j;
+        resolution = resolution;
+        pixelSize = (incX, incY)
+        pixelCentres = offsets
+    }
+
+let generateRays camera resolution =
+    let pixelGrid = createGrid camera resolution
     let rays = Seq.map (fun (x, y) ->
-        let via = addP (addP ahead (mulV x right)) (mulV -y up)
-        { o = camera.o; d = subP via camera.o }) offsets
+        let via = addP (addP pixelGrid.centre (mulV x pixelGrid.i)) (mulV y pixelGrid.j)
+        { o = camera.o; d = subP via camera.o }) (pixelGrid.pixelCentres |> Seq.cast<float * float>)
     rays
