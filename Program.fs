19913627a222eda1f659a595235f49a39e9274d7
Unknown <mjrmua@gmail.com>
2017-09-24 22:08:38 +0200

    Most basic directional lighting

--- Program.fs
+++ Program.fs
@@ -8,16 +8,13 @@ open Sphere
 open Plane
 open Image
 open Scene
+open Shading
 
-let normalToColour = function
-    | Some { n = Vector (x,y,z) } -> let remap c = (c + 1.0) / 2.0 * 255.0 |> byte in Colour (remap x, remap y, remap z)
-    | _ -> Colour (0uy, 0uy, 0uy)
 
-let shade scene (pixelRays : seq<(int * int) * Ray list>) =
+let shade shader scene (pixelRays : seq<(int * int) * Ray list>) =
     let shadePixel (rays : Ray list) =
-        let intersections = Seq.map (fun ray -> intersectScene scene ray) rays
-        // let intersections = Seq.map (fun ray -> async { return intersectScene scene ray }) rays |> Async.Parallel |> Async.RunSynchronously
-        let colours = Seq.map normalToColour intersections
+        let intersections = Seq.map (intersectScene scene) rays
+        let colours = Seq.map shader intersections
         let (fr, fg, fb) = Seq.fold (fun (ar, ag, ab) (Colour (r, g, b)) -> (ar + float r, ag + float g, ab + float b)) (0.0, 0.0, 0.0) colours
         let c = float rays.Length
         Colour (fr / c |> byte, fg / c |> byte, fb / c |> byte)
@@ -26,6 +23,7 @@ let shade scene (pixelRays : seq<(int * int) * Ray list>) =
 
 [<EntryPoint>]
 let main argv =
+    let shader = shadeOrBackground white directionalLightShader
     let s = Scene [
                   sphere (Point (0.0,0.0,10.0)) 4.0;
                   sphere (Point (1.0,3.0,10.0)) 4.0;
@@ -35,7 +33,7 @@ let main argv =
     let resolution = Resolution (400, 400)
     let c = { o = Point (0.0, 0.0, 0.0); up = Vector (0.0, 1.0, 0.0); lookAt = Point (0.0, 0.0, 1.0); fovY = System.Math.PI / 2.0; aspectRatio = 1.0 }
     let pixelRays = generateRays c resolution
-    let pixels = shade s pixelRays
+    let pixels = shade shader s pixelRays
     let b = { resolution = resolution; pixels = Seq.toList << Seq.map snd <| pixels }
     write b "test.ppm"
     0
--- Shading.fs
+++ Shading.fs
@@ -1,3 +1,26 @@
 module Shading
+open Ray;
+open Image;
+open Vector;
 
-type Colour = float * float * float
+let black = Colour (0uy, 0uy, 0uy)
+let white = Colour (255uy, 255uy, 255uy)
+
+let shadeOrBackground background shader = function
+    | Some fragment -> shader fragment
+    | _ -> background
+
+let clamp x = 
+        match x with
+            | _ when x > 255.0 -> 255uy
+            | _ when x<0.0     -> 0uy
+            | _                -> (byte) x
+
+let normalToColourShader { n = Vector (x,y,z) } = 
+    let remap c = (c + 1.0) / 2.0 * 255.0 |> byte in Colour (remap x, remap y, remap z)
+
+let directionalLightShader { n = normal } =
+    let lightDirection = Vector(1.0, -1.0, 3.0) |> negV |> normalise
+    let intensity = clamp (dot lightDirection normal * 255.0)
+    Colour(intensity, intensity, intensity);
+
