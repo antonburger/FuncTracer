8efe645364bddd8d16938e1d001e1bb0acc30e9b
Unknown <mjrmua@gmail.com>
2017-09-26 12:19:45 +0200

    TODONE: injected shader

--- Shading.fs
+++ Shading.fs
@@ -5,37 +5,52 @@ open Vector;
 open Light
 open Scene;
 
-let lightShader light intersectedObject =
-    match light with
-    | (DirectionalLight v, lightColour) ->
-        let normal = intersectedObject.intersection.n
-        let colour = intersectedObject.sceneObject.Material.colour * lightColour
-        scaleColour (-v .* normal) colour
+type Fragment  = {
+    intersectedObject: IntersectedObject; 
+    scene: Scene
+}
+
+type Shader = (Fragment -> Colour)
+
+let shadeOrBackground background shader = function 
+    | Some fragment -> shader fragment 
+    | _ -> background 
 
 // Hard shadow determination - in or out. Probably want to change this to a shadow factor when there are area lights.
 let isInShadow scene light point =
     match light with
-    | DirectionalLight v ->
-        intersectsAny scene { o = point; d = -v }
+    | DirectionalLight v -> intersectsAny scene { o = point; d = -v }
 
-// TODO: Find a way to re-enable passing a custom shader here.
-let shade scene (pixelRays : seq<(int * int) * Ray list>) =
-    let shadeRay ray =
-        let o = intersectScene scene ray
-        match o with
-        | None -> Colour.black
-        | Some ({ intersection = i } as o2) ->
-            // Arbitrary offset to combat shadow artifacts.
-            let shadowRayOrigin = i.p + i.n * 0.0001
-            scene.lights |>
-            Seq.map (fun l -> if isInShadow scene (fst l) shadowRayOrigin then Colour.black else lightShader l o2) |>
-            // Accumulate and clamp the colours per light.
-            Seq.fold (+) Colour.black |>
-            Colour.map clamp
-    let shadePixel (rays : Ray list) =
-        rays |>
-        Seq.map shadeRay |>
-        // Average the ray colours per pixel.
-        Seq.fold (+) Colour.black |>
-        Colour.map (fun c -> c / float rays.Length)
-    Seq.map (fun pixelRay -> async { return (fst pixelRay, shadePixel <| snd pixelRay) }) pixelRays |> Async.Parallel |> Async.RunSynchronously
+let singleLightShader light intersectedObject = 
+    match light with 
+    | (DirectionalLight v, lightColour) -> 
+        let normal = intersectedObject.intersection.n 
+        let colour = intersectedObject.sceneObject.Material.colour * lightColour 
+        scaleColour (-v .* normal) colour 
+
+let lightShader fragment =
+    let intersectedObject = fragment.intersectedObject
+    let scene = fragment.scene
+    // Arbitrary offset to combat shadow artifacts. 
+    let i = fragment.intersectedObject.intersection;
+    let shadowRayOrigin = i.p + i.n * 0.0001 
+    scene.lights |> 
+    Seq.map (fun l -> if isInShadow scene (fst l) shadowRayOrigin then Colour.black else singleLightShader l intersectedObject) |> 
+    // Accumulate and clamp the colours per light. 
+    Seq.fold (+) Colour.black |> 
+    Colour.map clamp 
+
+
+let createFragment scene someIntersection = 
+    match someIntersection with 
+    | None -> None
+    | Some intersection -> Some { scene=scene; intersectedObject=intersection }
+
+let shade shader scene (pixelRays : seq<(int * int) * Ray list>) = 
+    let shadePixel (rays : Ray list) = 
+        rays |> 
+        Seq.map (intersectScene scene >> (createFragment scene) >> shader) |> 
+        Seq.fold (+) Colour.black |> 
+        Colour.map (fun c -> c / float rays.Length) 
+    Seq.map (fun pixelRay -> async { return (fst pixelRay, shadePixel <| snd pixelRay) }) pixelRays |> Async.Parallel |> Async.RunSynchronously 
+
