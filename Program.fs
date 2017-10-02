7a7dc6042d9a11552321a7c4805810afb8098987
SPerumal <sam_lki@yahoo.com>
2017-10-03 00:36:19 +0200

    Added simply GUI that monitors the build directory, and runs the tracer if changes are deteced.

--- Program.fs
+++ Program.fs
@@ -25,7 +25,7 @@ let duration f =
     let timer = System.Diagnostics.Stopwatch()
     timer.Start()
     let returnValue = f(timer)
-    printfn "Elapsed Time: %ims" timer.ElapsedMilliseconds
+    eprintfn "Elapsed Time: %ims" timer.ElapsedMilliseconds
     returnValue    
 
 //let addGeometry scene = 
@@ -39,49 +39,47 @@ let printIntersectionAt (x,y) =
     let imagePlane = createPlane options.camera options.resolution
     let ray=rayThroughPixel imagePlane (x,y) (0.0,0.0)
     let intersection = intersectScene scene ray
-    printfn "intersection:"
-    printfn "%A" intersection
+    eprintfn "intersection:"
+    eprintfn "%A" intersection
 
 let runTracer (timer:Diagnostics.Stopwatch, input:TextReader, output:Stream) = 
     let (options, scene) = readScene input
-    printfn "Parsed input %ims" timer.ElapsedMilliseconds
+    eprintfn "Parsed input %ims" timer.ElapsedMilliseconds
     let (pixelRays, count) = generateRays options.camera options.multisampleCount options.resolution
-    printfn "Generated %i rays: %ims" count timer.ElapsedMilliseconds
+    eprintfn "Generated %i rays: %ims" count timer.ElapsedMilliseconds
     let shader = multiPartShader [specularShader; reflectionShader; diffuseShader]
-    printfn "Created shader: %ims" timer.ElapsedMilliseconds
+    eprintfn "Created shader: %ims" timer.ElapsedMilliseconds
     let pixels = shade shader scene pixelRays
-    printfn "Shaded scene %ims" timer.ElapsedMilliseconds
+    eprintfn "Shaded scene %ims" timer.ElapsedMilliseconds
     let bitmap = { resolution = options.resolution; pixels = Seq.toList << Seq.map snd <| pixels }
-    printfn "Writing output %ims" timer.ElapsedMilliseconds
+    eprintfn "Writing output %ims" timer.ElapsedMilliseconds
     write bitmap output 
     0
 
 let getOutputStream (args : string[]) : Stream = 
     match args.Length with
+    // | 1 -> 
+    //     eprintfn "Using default output file: test.png"        
+    //     new FileStream("test.png" , FileMode.Create, FileAccess.Write, FileShare.None) :> Stream
     | 2 -> 
-        printfn "Using output file: %s" args.[1]
+        eprintfn "Using output file: %s" args.[1]
         new FileStream(args.[1] , FileMode.Create, FileAccess.Write, FileShare.None) :> Stream
-    | 1 -> 
-        printfn "Using standard output"
-        Console.OpenStandardOutput() 
     | _ -> 
-        printfn "Using default output file: test.png"        
-        new FileStream("test.png" , FileMode.Create, FileAccess.Write, FileShare.None) :> Stream
-
+        eprintfn "Using standard output"
+        Console.OpenStandardOutput() 
+    
 let getInputStream (args : string[]) : TextReader =
     match args.Length with
     | 0 -> 
-        printfn "Using default input file: sample.scene"
-        file "sample.scene" :> TextReader
-    | 1 -> 
-        printfn "Using standard input"
+        eprintfn "Using standard input"
         Console.In 
     | _ -> 
-        printfn "Using input file: %s" args.[0]
+        eprintfn "Using input file: %s" args.[0]
         file args.[0] :> TextReader    
 
 [<EntryPoint>]
 let main (args) =
+    eprintfn "Arguments: %s" args.[0]
     use input = getInputStream args
     use output = getOutputStream args
     duration (fun timer -> runTracer (timer, input, output))
