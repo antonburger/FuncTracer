cf482669b649cca92ba1939fa1489e96feb136a4
Unknown <mjrmua@gmail.com>
2017-10-13 16:48:36 +0200

    Texture coordinates for spheres

--- Sphere.fs
+++ Sphere.fs
@@ -3,6 +3,11 @@ module Sphere
 open Ray
 open Vector
 
+let private setUV r = 
+    let (Vector(dx, dy, dz)) = -r.n
+    let u = 0.5 + ((atan2 dz dx)/(2.0*System.Math.PI))
+    let v = 0.5 - (asin dy)/System.Math.PI
+    {r with uv = (u,v)}
 let sphere r =
     let ov = Point.toVector r.o
     let a = r.d .* r.d
@@ -10,6 +15,7 @@ let sphere r =
     let c = (ov .* ov) - 1.0
     let intersection t =
         let p = r.o + t * r.d
-        { newIntersection with  t = t; p = p; n = Point.toVector p |> normalise }
+        { newIntersection with  t = t; p = p; n = Point.toVector p |> normalise } 
+        |> setUV
     Math.quadratic a b c |>
-    Seq.map intersection
+    Seq.map intersection 
